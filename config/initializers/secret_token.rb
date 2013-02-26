155364e40ce793b213feafcc4d22088543581764
Amin Ogarrio <amin.ogarrio@gmail.com>
2013-02-26 00:17:27 -0700

    start project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pichontter::Application.config.secret_token = 'e101f040db9b35a442ea70c5492484980aa35a8ca3eda8fdc8d4eed5d41fc87ca1e5eb4dbbfe7da06d0bb1413eee32ba107733838651de2d7143c02fb804a73e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pichontter::Application.config.session_store :cookie_store, key: '_pichontter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pichontter::Application.config.session_store :active_record_store
