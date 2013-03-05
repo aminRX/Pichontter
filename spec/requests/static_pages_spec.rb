ecebe716641c9a1b8d24c47c6739ab1dee355443
Amin Ogarrio <amin.ogarrio@gmail.com>
2013-03-04 20:15:49 -0700

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,23 +3,43 @@ require 'spec_helper'
 describe "Static pages" do
 
 	describe "Home page" do
-		it "should have content 'Pichontter'" do
+
+		it "should have the h1 'Pichontter'" do
 			visit '/static_pages/home'
-			page.should have_content('Pichontter')
+			page.should have_selector('h1',
+				:text => 'Pichontter')
+		end
+
+		it "should have right title" do
+			visit '/static_pages/home'
+			page.should have_selector('title',
+				:text => ' | Home')
 		end
 	end
 
 	describe "Help page" do
-		it "should have content 'Help'" do
+		it "should have the h1 'Help'" do
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			page.should have_selector('h1',
+				:text => 'Help')
+		end
+		it "should have right title" do
+			visit '/static_pages/help'
+			page.should have_selector('title',
+				:text => ' | Help')
 		end
 	end
 
 	describe "About page" do
-		it "should have content 'About'" do
+		it "should have the h1 'About Us'" do
 			visit '/static_pages/about'
-			page.should have_content('About Us')
+			page.should have_selector('h1',
+				:text => 'About Us')
+		end
+		it "should have right title" do
+			visit '/static_pages/about'
+			page.should have_selector('title',
+				:text => ' | About Us')
 		end
 	end
 end
