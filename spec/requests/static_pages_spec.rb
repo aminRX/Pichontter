9a38546d342e264f17708df56113278b32dbcbe6
Amin Ogarrio <amin.ogarrio@gmail.com>
2013-03-05 23:33:34 -0700

    add contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,13 +7,13 @@ describe "Static pages" do
 		it "should have the h1 'Pichontter'" do
 			visit '/static_pages/home'
 			page.should have_selector('h1',
-				:text => 'Pichontter')
+									:text => 'Pichontter')
 		end
 
 		it "should have right title" do
 			visit '/static_pages/home'
 			page.should have_selector('title',
-				:text => ' | Home')
+									:text => ' | Home')
 		end
 	end
 
@@ -21,12 +21,12 @@ describe "Static pages" do
 		it "should have the h1 'Help'" do
 			visit '/static_pages/help'
 			page.should have_selector('h1',
-				:text => 'Help')
+									:text => 'Help')
 		end
 		it "should have right title" do
 			visit '/static_pages/help'
 			page.should have_selector('title',
-				:text => ' | Help')
+									:text => ' | Help')
 		end
 	end
 
@@ -34,12 +34,24 @@ describe "Static pages" do
 		it "should have the h1 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_selector('h1',
-				:text => 'About Us')
+									:text => 'About Us')
 		end
 		it "should have right title" do
 			visit '/static_pages/about'
 			page.should have_selector('title',
-				:text => ' | About Us')
+									:text => ' | About Us')
+		end
+	end
+	describe "Contact" do
+		it "should have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1',
+				           			:text => 'Contact')
+		end
+		it "should have right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+									:text => ' | Contact')
 		end
 	end
 end
